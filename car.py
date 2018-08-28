911e06f26f4dd42bd136e31f2e4854fe310f3b65
Zachkery <zachkery999@gmail.com>
2018-08-28 11:45:05 -0500

    Method within init
    Added a method within init to be called on once an object is created. This will display all the values of the object's attributes once it's created.

--- car.py
+++ car.py
@@ -8,6 +8,7 @@ class Car(object):
             self.tax = 0.12
         else:
             self.tax = 0.15
+        self.display_all()
     
     def display_all(self):
         print 'Price:'+str(self.price)
@@ -17,12 +18,12 @@ class Car(object):
         print 'Tax:'+str(self.tax)
         print '*'* 50
  
-car1 = Car(2500, 35, 'Full', 15).display_all()
-car2 = Car(7591, 5, 'Not Full', 105).display_all()
-car3 = Car(10000, 15, 'Kind of Full', 95).display_all()
-car4 = Car(2000, 25, 'Full', 25).display_all()
-car5 = Car(25000, 45, 'Full', 85).display_all()
-car6 = Car(1170, 45, 'Empty', 15).display_all()
+car1 = Car(2500, 35, 'Full', 15)
+car2 = Car(7591, 5, 'Not Full', 105)
+car3 = Car(10000, 15, 'Kind of Full', 95)
+car4 = Car(2000, 25, 'Full', 25)
+car5 = Car(25000, 45, 'Full', 85)
+car6 = Car(1170, 45, 'Empty', 15)
 
 # print car1.display_all(), car2.display_all(), car3.display_all(), car4.display_all(), car5.display_all(), car6.display_all()
 
